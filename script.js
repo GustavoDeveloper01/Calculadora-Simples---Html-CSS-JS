6a80cb119002d83bcc44e65e4543326b29414c2f
GustavoDeveloper01 <109388163+GustavoDeveloper01@users.noreply.github.com>
2022-09-12 11:42:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function displaynum(n1) {
+    calculator.text1.value = calculator.text1.value + n1 ;
+}
+function getAndShowKeys(button){
+    document.getElementById('screenResult').value = button.value;
+}
